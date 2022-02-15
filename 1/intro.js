b0bec23737c03be16cb3e2d12d5ccf4b3f0aed36
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-02-16 01:17:09 +0530

    1

---
+++ 1/intro.js
@@ -0,0 +1,22 @@
+What is server? 
+
+The main brain of web app is server.
+These are of two types - 
+1. Software - Protocols 
+2. Hadware - Harddisk, Database
+
+
+Advantages of server 
+1. Efficient storage and delivery of information 
+2. Customised user expxerience  - creditcard information SVGAElement, and unique suggestions 
+according  to the Intrest. 
+3. Controlled access of Contents. - Facilities of paid websites 
+4. Notifications and COmmunications. - like fb and amazon
+
+
+HTTP 
+Hypertext       transfer     protocol
+     |              |            |
+	       transfer krne ka set of rules
+
+HTML -> Hypertext markup language 
