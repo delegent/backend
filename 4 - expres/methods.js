a7c39b3e8962eb3290505022d9cd95936da1f75e
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-02-26 00:00:33 +0530

    SIGNUP_START

--- 4 - expres/methods.js
+++ 4 - expres/methods.js
@@ -5,100 +5,114 @@ const app = express();
 // middleware function for post 
 app.use(express.json());
 
+const authRouter  = express.Router();
+app.use('/auth',authRouter);
+
+authRouter.
+route('/signup')
+.get(getSignUp)
+.post(postSignUp)
+
+function getSignUp(req,res){
+	res.sendFile('');
+}
+
+
+
 app.listen(3000);
 
 
 
 
-let users = [
-	{
-		'id':1,
-		'name':'Anubhav'
-	},
-	{
-		'id':2,
-		'name':'Samar'
-	},
-	{
+// let users = [
+// 	{
+// 		'id':1,
+// 		'name':'Anubhav'
+// 	},
+// 	{
+// 		'id':2,
+// 		'name':'Samar'
+// 	},
+// 	{
 
-		'id':3,
-		"name":"Vaibhav"
+// 		'id':3,
+// 		"name":"Vaibhav"
 
-	}
-];
- // mounting 
-const userRouter = express.Router();
-app.use('/user',userRouter);
-userRouter.route(/)
-.get(getUser)
-.post(postUser)
-.patch(updateUser)
-.delete(deleteUser)
+// 	}
+// ];
+//  // mounting 
+// const userRouter = express.Router();
+// app.use('/user',userRouter);
+// userRouter.route(/)
+// .get(getUser)
+// .post(postUser)
+// .patch(updateUser)
+// .delete(deleteUser)
 
-app.get('/user',function(req,res){
-	console.log(req.query)
-	res.send(users);
-})
-
-
-app.post('/user',function(req,res){
-	console.log(req.body);
-	user = req.body.name
-	res.json({
-		"message":"data recieved successfully",
-		"data":user
-	})
-})
+// app.get('/user',function(req,res){
+// 	console.log(req.query)
+// 	res.send(users);
+// })
 
 
-app.patch('/user',function(req,res){
-	console.log(
-		req.body
-	);
+// app.post('/user',function(req,res){
+// 	console.log(req.body);
+// 	user = req.body.name
+// 	res.json({
+// 		"message":"data recieved successfully",
+// 		"data":user
+// 	})
+// })
 
 
-// update data in users object\
-let updatedData = req.body;
-for(val in updatedData){
-users[val] = updatedData[val];
-}
+// app.patch('/user',function(req,res){
+// 	console.log(
+// 		req.body
+// 	);
+
+
+// // update data in users object\
+// let updatedData = req.body;
+// for(val in updatedData){
+// users[val] = updatedData[val];
+// }
 		
 
 
-	res.json(
-		{
-			"message":"data recieved successfully"
-		}
-	)
-})
+// 	res.json(
+// 		{
+// 			"message":"data recieved successfully"
+// 		}
+// 	)
+// })
 
 
 
-/// params
-app.get('/user/:userName',function(req,res){
-	console.log(req.params.userName);
+// /// params
+// app.get('/user/:userName',function(req,res){
+// 	console.log(req.params.userName);
 	
-	// console.log(req.params.id);
-	res.json({"data recieved":req.params.userName});
+// 	// console.log(req.params.id);
+// 	res.json({"data recieved":req.params.userName});
 
-})
+// })
 
 
-function getUser(){
+// function getUser(){
 
-}
+// }
 
-function postUser(){
+// function postUser(){
 
-}
+// }
 
-function updateUser(){
+// function updateUser(){
 
-}
+// }
 
-function deleteUser(){
+// function deleteUser(){
 
-}
+// }
 
 
 
